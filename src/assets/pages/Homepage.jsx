51636435112c754b5abd318a6fb7aee416c96f79
Johan Sierra <johansierra222@gmail.com>
2025-07-07 23:11:54 -0500

    feat: homepage UI finished

--- src/assets/pages/Homepage.jsx
+++ src/assets/pages/Homepage.jsx
@@ -11,8 +11,8 @@ const Homepage = () => {
       <div className=" z-10 text-white flex items-center justify-center">
         {/* Aquí va el contenido de tu sitio */}
         <div className="flex flex-col justify-center items-center absolute top-[50%] left-[37%] transform -translate-x-1/2 -translate-y-1/2 text-4xl font-medium">
-          <h1>Simuladores de Ondas</h1>
-          <div className="mt-10 flex gap-10">
+          <h1 className="whitespace-nowrap">Simuladores de Ondas</h1>
+          <div className="mt-10 flex items-center justify-center flex-wrap gap-7">
             <SimulationCards title="Ondas Sonoras" src={Waves}/>
             <SimulationCards title="Ondas Electromagnéticas" src={Color}/>
           </div>
--- src/components/SimulationCard.jsx
+++ src/components/SimulationCard.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const SimulationCard = ({title, src}) => {
   return (
-    <div className='text-center w-fit px-15 pt-10 pb-6 bg-white rounded-[1.5rem]'>
+    <div className='cursor-pointer text-center w-fit px-15 pt-10 pb-6 bg-white rounded-[1.5rem] hover:opacity-86'>
       <img className="m-auto"src={src} alt={`${title} image`}/>
-      <p className='mt-5 whitespace-nowrap text-black text-2xl inline-flex font-semibold'>{title}</p>
+      <p className='mt-5 whitespace-nowrap text-black text-xl inline-flex font-semibold'>{title}</p>
     </div>
   )
 }
