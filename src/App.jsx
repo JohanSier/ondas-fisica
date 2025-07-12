b6387b0fa792ce5811ecfe032bbf24d39a4c37e0
Johan Sierra <johansierra222@gmail.com>
2025-07-12 15:59:54 -0500

    feat: add router with three pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-// App.jsx o donde quieras mostrar el fondo animado
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from './assets/pages/Homepage';
+import LightLab from "./assets/pages/LightLab";
+import SoundLab from "./assets/pages/SoundLab";
 
 function App() {
   return (
-    <>
-      <Homepage/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route index path="/" element={<Homepage/>}/>
+      <Route index path="simulador-sonido" element={<SoundLab/>}/>
+      <Route index path="simulador-frecuencias" element={<LightLab/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
