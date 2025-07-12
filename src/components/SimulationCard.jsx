bd5cacf7000921d4491429a32e51394f222b7e31
Johan Sierra <johansierra222@gmail.com>
2025-07-12 16:01:11 -0500

    feat: add prop to add teh function

--- src/components/SimulationCard.jsx
+++ src/components/SimulationCard.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-
-const SimulationCard = ({title, src}) => {
+const SimulationCard = ({title, src, click}) => {
   return (
-    <div className='cursor-pointer text-center w-fit px-15 pt-10 pb-6 bg-white rounded-[1.5rem] hover:opacity-86'>
+    <div onClick={click} className='cursor-pointer text-center w-fit px-15 pt-10 pb-6 bg-white rounded-[1.5rem] hover:opacity-86'>
       <img className="m-auto"src={src} alt={`${title} image`}/>
       <p className='mt-5 whitespace-nowrap text-black text-xl inline-flex font-semibold'>{title}</p>
     </div>
